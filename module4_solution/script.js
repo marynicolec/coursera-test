21500d148e6231ace731edaec8d2de6d099374c9
MARY CHAMBERS <mnchambe@us.ibm.com>
2019-12-22 06:50:15 -0600

    Module 4 Javascript

--- module4_solution/script.js
+++ module4_solution/script.js
@@ -51,8 +51,8 @@ console.log(names);
   // string object's 'toLowerCase' method on the result so we can compare
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
-   //  var firstLetter = names.charAt(0);
-  //   console.log(firstLetter);
+     var firstLetter = names.charAt(0);
+     console.log(firstLetter);
   // STEP 12:
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
