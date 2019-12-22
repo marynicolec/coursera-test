061ef11ff0077c1a9e828fea692d69454187838f
MARY CHAMBERS <mnchambe@us.ibm.com>
2019-12-22 06:35:02 -0600

    Module 4 Javascript

--- module4_solution/script.js
+++ module4_solution/script.js
@@ -28,7 +28,7 @@ assignment.
 WARNING!!! WARNING!!!
 
 */
-(function (window) {
+//function (window) {
   // body...
 
 // STEP 1:
@@ -36,15 +36,14 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+//var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
 // 'speak' method.
 // See Lecture 50, part 1
-for (var i = 0; i < names.length; i++) {
-
+//for (var i = 0; i < names.l
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
   // Use the string object's 'charAt' function. Since we are looking for
@@ -52,18 +51,23 @@ for (var i = 0; i < names.length; i++) {
   // string object's 'toLowerCase' method on the result so we can compare
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
-     var firstLetter = names.charAt(0);
-
+   //  var firstLetter = names.charAt(0);
+  //   console.log(firstLetter);
   // STEP 12:
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if ((firstLetter.toLowerCase()) == 'j') {
-    byeSpeaker.speak = function (names);
+  /*
+  if (firstLetter=='j'){
+  //if ((firstLetter.toLowerCase()) == 'j') {
+    //byeSpeaker.speak = function (names);
+     console.log("IF");
   } else {
      helloSpeaker.speak = function (names);
   }
-}
+  */
+//}
 
-})(window);
+//})(window);
+console.log("Hello");
