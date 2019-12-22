934742965ddedddf04d98cebdf2524ef7e7b8560
MARY CHAMBERS <mnchambe@us.ibm.com>
2019-12-22 08:01:04 -0600

    Module 4 Javascript

--- module4_solution/script.js
+++ module4_solution/script.js
@@ -37,13 +37,13 @@ WARNING!!! WARNING!!!
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-console.log(names);
+console.log(names.length);
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
 // 'speak' method.
 // See Lecture 50, part 1
-   for (var i = 0; i < names.length(); i++){
+   for (var i = 0; i < names.length; i++){
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
   // Use the string object's 'charAt' function. Since we are looking for
