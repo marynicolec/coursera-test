26902a7c04ea91e916b09b13f21fac4421b1420e
MARY CHAMBERS <mnchambe@us.ibm.com>
2019-12-22 15:16:17 -0600

    Module 4 Javascript

--- module4_solution/script.js
+++ module4_solution/script.js
@@ -32,7 +32,7 @@ WARNING!!! WARNING!!!
 (function (window) {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-console.log(names.length());
+console.log(names.length);
 
    for (var i = 0; i < names.length; i++){
 
