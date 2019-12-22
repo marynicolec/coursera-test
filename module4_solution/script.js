6286d344ddc50194800942c9d1c850bb3ce4c11e
MARY CHAMBERS <mnchambe@us.ibm.com>
2019-12-22 17:34:05 -0600

    Module 4 Javascript

--- module4_solution/script.js
+++ module4_solution/script.js
@@ -34,8 +34,7 @@ WARNING!!! WARNING!!!
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 console.log(names.length);
 
-   for (var i = 0; i < names.length(); i++){
-
+   for (var i = 0; i < names.length; i++){
      var firstLetter = names[i].charAt(0);
      var x = firstLetter.toLowerCase();
      console.log("Inside Loop");
