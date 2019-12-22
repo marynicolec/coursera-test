d2dea7c5925e487144cc41a5411d5012b8d6a77b
MARY CHAMBERS <mnchambe@us.ibm.com>
2019-12-22 06:37:23 -0600

    Module 4 Javascript

--- module4_solution/script.js
+++ module4_solution/script.js
@@ -36,14 +36,14 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
-//var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+console.log(names);
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
 // 'speak' method.
 // See Lecture 50, part 1
-//for (var i = 0; i < names.l
+//for (var i = 0; i < names
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
   // Use the string object's 'charAt' function. Since we are looking for
@@ -70,4 +70,3 @@ WARNING!!! WARNING!!!
 //}
 
 //})(window);
-console.log("Hello");
